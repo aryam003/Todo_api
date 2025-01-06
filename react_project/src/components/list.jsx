955c33822591e9f394f7f31a2a3e7369d52c18e2
aryam003 <aryamarya537@gmail.com>
2025-01-06 13:29:53 +0530

    work

--- react_project/src/components/list.jsx
+++ react_project/src/components/list.jsx
@@ -1,23 +1,27 @@
 import { useEffect,useState } from "react"
+import axios from 'axios'
 function List(){
     const [data,setData]=useState([])
     useEffect(()=>{
-        axios.get()
-    })
+        axios.get().then((res)=>{
+            console.log(res.data);
+            setData(res.data)
+        }).catch(error=>console.log(error.message))
+    },[])
     return(
         <div className="container">
             <h1>Diaplay Details</h1>
             <table className="table">
                 <thead>
                     <tr>
-                        <th>Title</th>
+                        <th>Task</th>
                         <th>Description</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((value,index)=>(
                         <tr key={index}>
-                            <td>{value.title}</td>
+                            <td>{value.task}</td>
                             <td>{value.description}</td>
                         </tr>
                     ))}
